bb9d9b595e4abb50ee62a688d80c56ed1718b06b
xialan <812070682@qq.com>
2023-03-29 17:10:57 +0800

    添加项目文件。

---
+++ HelloWord/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloWord
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
